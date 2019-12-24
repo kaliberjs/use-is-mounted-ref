12eaeb555d85b99bfe0eba643b873f34d77533d6
Joost Kiens <me@joostkiens.com>
2019-12-24 16:41:02 +0100

    improve example

--- example/src/domain/App.js
+++ example/src/domain/App.js
@@ -1,7 +1,7 @@
 import { useIsMountedRef } from '@kaliber/use-is-mounted-ref'
 
 export default function App() {
-  const [, set] = React.useState(false)
+  const [state, setState] = React.useState('⬇️')
   const isMountedRef = useIsMountedRef()
 
   // Use it to check if a component is still mounted after calling an async function.
@@ -12,22 +12,18 @@ export default function App() {
           await someAsyncFunction()
         } catch (err) {
           console.error(err)
+          if (isMountedRef.current) setState('⚠️')
         }
-        if (isMountedRef.current) { console.log('I\'m still mounted!') }
+        if (isMountedRef.current) setState('⬆️')
       }
       doSomethingAync()
     },
     [isMountedRef]
   )
 
-  // Or use it to see if a component is mounted, a functional equivalent of `componentDidMount`
   return (
     <>
-      <div style={{ fontSize: '5em' }}>{isMountedRef.current ? '⬆️' : '⬇️'}</div>
-      {!isMountedRef.current
-        // calling set with a new value triggers a rerender
-        && <button onClick={() => set(true)}>Mount</button>
-      }
+      <div style={{ fontSize: '5em' }}>{state}</div>
     </>
   )
 }
